58c296601a9606e495fb8a400c55c47d3417fefa
Emily <emilykiddell@gmail.com>
2022-04-04 16:41:53 +1000

    working on form error and success notifications

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-
-
-//change thumbnail images on button click
+//change colourway for each design
 
 function option(prm) {
     let optionOne = document.getElementById("option-1");
@@ -134,41 +132,23 @@ if (e.target !== e.currentTarget) {
     let textBlock1 = document.getElementById('text-block-1');
     let textBlock2 = document.getElementById('text-block-2');
     let textBlock3 = document.getElementById('text-block-3');
+    let insertBr = document.getElementById('date-output');
     let textBlock4 = document.getElementById('text-block-4');
     let textBlock5 = document.getElementById('text-block-5');
     let textBlock6 = document.getElementById('text-block-6');
-/*
-    textBlock1.classList.add('design-1-text');
-    textBlock2.classList.add('design-1-text');
-    textBlock3.classList.add('design-1-text');
-    textBlock4.classList.add('design-1-text');
-    textBlock5.classList.add('design-1-text');
-    textBlock6.classList.add('design-1-text');
 
-    textBlock1.classList.remove('design-2-text');
-    textBlock2.classList.remove('design-2-text');
-    textBlock3.classList.remove('design-2-text');
-    textBlock4.classList.remove('design-2-text');
-    textBlock5.classList.remove('design-2-text');
-    textBlock6.classList.remove('design-2-text');
-
-    textBlock1.style.display = 'block';
-    textBlock2.style.display = 'block';
-    textBlock3.style.display = 'block';
-    textBlock4.style.display = 'block';
-    textBlock5.style.display = 'block';
-    textBlock6.style.display = 'block';
-
-*/
         textBlock1.style.margin = '3.1rem 7.5rem 0 5.6rem';
         textBlock1.style.fontSize = '1.5rem';
         textBlock1.style.textAlign = 'left';
 
         textBlock2.style.margin = '.1rem 8.43rem 0 5.6rem';
         textBlock2.style.fontSize = '.9rem';
+        textBlock2.style.lineHeight = '1.1rem'
 
         textBlock3.style.margin = '.1rem 8.43rem 0 5.6rem';
         textBlock3.style.textAlign = 'left';
+
+        insertBr.style.display = 'inline';
 
         textBlock4.style.margin = '.1rem 8.43rem 0 5.6rem';
         textBlock4.style.textAlign = 'left';
@@ -176,7 +156,6 @@ if (e.target !== e.currentTarget) {
         textBlock5.style.margin = '.1rem 8.43rem 0 5.6rem';
         textBlock5.style.fontSize = '.9rem';
         textBlock5.style.textAlign = 'left';
-        
 
         textBlock6.style.margin = '0 0 0 9.3rem';
         textBlock6.style.textAlign = 'right';
@@ -187,7 +166,6 @@ if (e.target !== e.currentTarget) {
     }
     e.stopPropagation();
 }
-
 
 //change font and text for bauhaus heart design (options 3 and 4)
 
@@ -203,53 +181,31 @@ if (e.target !== e.currentTarget) {
     let textBlock1 = document.getElementById('text-block-1');
     let textBlock2 = document.getElementById('text-block-2');
     let textBlock3 = document.getElementById('text-block-3');
+    let insertBr = document.getElementById('date-output');
     let textBlock4 = document.getElementById('text-block-4');
     let textBlock5 = document.getElementById('text-block-5');
     let textBlock6 = document.getElementById('text-block-6');
 
-    /*
-
-    textBlock1.classList.add('design-2-text');
-    textBlock2.classList.add('design-2-text');
-    textBlock3.classList.add('design-2-text');
-    textBlock4.classList.add('design-2-text');
-    textBlock5.classList.add('design-2-text');
-    textBlock6.classList.add('design-2-text');
-
-    textBlock1.classList.remove('design-1-text');
-    textBlock2.classList.remove('design-1-text');
-    textBlock3.classList.remove('design-1-text');
-    textBlock4.classList.remove('design-1-text');
-    textBlock5.classList.remove('design-1-text');
-    textBlock6.classList.remove('design-1-text');
-    
-    textBlock1.style.display = 'block';
-    textBlock2.style.display = 'block';
-    textBlock3.style.display = 'block';
-    textBlock4.style.display = 'block';
-    textBlock5.style.display = 'block';
-    textBlock6.style.display = 'block';
-
-    */
-
-        textBlock1.style.margin = '5.4rem 7.5rem 0 8.43rem';
+        textBlock1.style.margin = '5.5rem 7.5rem 0 8.43rem';
         textBlock1.style.fontSize = '1.25rem';
 
-        textBlock2.style.margin = '.4rem 7.5rem 0 8.43rem';
+        textBlock2.style.margin = '.5rem 7.5rem 0 8.43rem';
         textBlock2.style.fontSize = '.7rem';
-        textBlock2.style.fontStyle = 'italic';
+        textBlock2.style.fontStyle = 'normal';
+        textBlock2.style.lineHeight = '.85rem';
 
-        textBlock3.style.margin = '.4rem 7.5rem 0 8.43rem';
+        textBlock3.style.margin = '.4rem 8.5rem 0 8.43rem';
         textBlock3.style.textAlign = 'left';
+
+        insertBr.style.display = 'block';
 
         textBlock4.style.margin = '.4rem 7.5rem 0 8.43rem';
         textBlock4.style.textAlign = 'left';
 
         textBlock5.style.margin = '.4rem 7.5rem 0 8.43rem';
         textBlock5.style.fontSize = '.7rem';
-        textBlock5.style.fontStyle = 'italic';
-        textBlock4.style.textAlign = 'left';
-        
+        textBlock5.style.fontStyle = 'normal';
+        textBlock5.style.textAlign = 'left';
 
         textBlock6.style.margin = '.4rem 7.5rem 0 8.43rem';
         textBlock6.style.textAlign = 'left';
@@ -274,22 +230,25 @@ if (e.target !== e.currentTarget) {
     let textBlock1 = document.getElementById('text-block-1');
     let textBlock2 = document.getElementById('text-block-2');
     let textBlock3 = document.getElementById('text-block-3');
+    let insertBr = document.getElementById('date-output');
     let textBlock4 = document.getElementById('text-block-4');
     let textBlock5 = document.getElementById('text-block-5');
     let textBlock6 = document.getElementById('text-block-6');
 
-        textBlock1.style.margin = '2.3rem 5.5rem 0 5.5rem';
+        textBlock1.style.margin = '2.25rem 5.5rem 0 5.5rem';
         textBlock1.style.fontSize = '1.2rem';
 
-        textBlock2.style.margin = '0 5.5rem 0 5.5rem';
+        textBlock2.style.margin = '.05rem 5.5rem 0 5.5rem';
         textBlock2.style.fontSize = '.9rem';
         textBlock2.style.fontStyle = 'normal';
         textBlock2.style.textAlign = 'left';
         textBlock2.style.lineHeight = '1.35rem';
 
-        textBlock3.style.margin = '7.8rem 9rem 0 9rem';
+        textBlock3.style.margin = '7.8rem 7rem 0 7rem';
         textBlock3.style.fontSize = '1rem';
         textBlock3.style.textAlign = 'center';
+
+        insertBr.style.display = 'inline';
 
         textBlock4.style.margin = '0.2rem 7.5rem 0 7.5rem';
         textBlock4.style.fontSize = '1rem';
@@ -300,7 +259,7 @@ if (e.target !== e.currentTarget) {
         textBlock5.style.fontStyle = 'regular';
         textBlock5.style.textAlign = 'center';
 
-        textBlock6.style.margin = '0.2rem 8rem 0 8rem';
+        textBlock6.style.margin = '0.2rem 8.2rem 0 8.2rem';
         textBlock6.style.textAlign = 'center';
         textBlock6.style.position = 'initial';
         textBlock6.style.fontSize = '.6rem';
@@ -308,22 +267,7 @@ if (e.target !== e.currentTarget) {
     e.stopPropagation();
 }
 
-//form
-
-/*
-function livePrintForm(input, output) {
-    document.getElementById(input).onkeyup = function() {
-        document.getElementById(output).textContent = this.value;
-    };
-}
-
-        livePrintForm('namep1', 'name-1-output');
-        livePrintForm('namep2', 'name-2-output');
-        livePrintForm('date-time', 'event-time-output');
-        livePrintForm('location', 'location-output');
-        livePrintForm('reception', 'reception-output');
-        livePrintForm('rsvp', 'rsvp-output');
-*/
+//live print
 
 function livePrintP1() {
     let nameP1 = document.getElementById('nameP1').value;
@@ -370,3 +314,52 @@ function livePrintRsvp() {
     let rsvpOutput = document.getElementById('rsvp-output');
     return rsvpOutput.textContent = `${rsvp}`;
 }
+
+//form notifications
+
+const exportDesign = document.getElementById('export-btn');
+const nameP1 = document.getElementById('nameP1');
+const nameP2 = document.getElementById('nameP2');
+const date = document.getElementById('date');
+const time = document.getElementById('time');
+const location = document.getElementById('location');
+const reception = document.getElementById('reception');
+const rsvp = document.getElementById('rsvp');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const nameP1Value = nameP1.value.trim();
+    const nameP2Value = nameP2.value.trim();
+    const dateValue = date.value.trim();
+    const timeValue = time.value.trim();
+    const locationValue = location.value.trim();
+    const receptionValue = reception.value.trim();
+    const rsvpValue = rsvp.value.trim();
+
+    if (nameP1Value == "" /*|| nameP1Value.length > 22*/) {
+        //show error
+        //add error class
+        setErrorFor(nameP1, 'Please fill in this field (1-22 characters)');
+    } else {
+        setSuccessFor(nameP1);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelecter('small');
+
+    small.innerText = message;
+    formControl.className = 'form-control error';
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+      
